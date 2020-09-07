9b3106c1ab807429839729d842ba55001a95901f
patrick-coda <louter.p.h.a@gmail.com>
2020-09-07 11:54:08 +0200

    Made input fields for the chat for name and chatroom

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "react-router-dom";
 
 export default function Join() {
-  return <div>This will be the room to join a room</div>;
+  const [name, set_name] = useState("");
+  const [room, set_room] = useState("");
+
+  return (
+    <div className="joinOuterContainer">
+      <div className="JoinInnerContainer">
+        <h1 classname="heading">fill this in to continue:</h1>
+        <div>
+          <input
+            placeholder="User name"
+            classname="joininput"
+            type="text"
+            onChange={(event) => set_name(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Chatroom"
+            classname="joininput"
+            type="text"
+            onChange={(event) => set_room(event.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }

3f7200db3af98e30a7008022f4d666c859b1c902
patrick-coda <louter.p.h.a@gmail.com>
2020-09-07 10:20:55 +0200

    Made the page where the caht app will be

--- src/pages/NearbyVets/NearbyVets.js
+++ src/pages/NearbyVets/NearbyVets.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NearbyVets() {
   return (
     <div>
-      <h1>This will be the nearbyvets page</h1>
+      <h1>This will be the chat wit other users page</h1>
     </div>
   );
 }

20ae2e692040433d5a350a3b688e54e6c3eb09f5
patrick-coda <louter.p.h.a@gmail.com>
2020-09-07 12:22:27 +0200

    imported the data from the join component

--- src/components/Chatroom/Chatroom.js
+++ src/components/Chatroom/Chatroom.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-export default function Chatroom() {
+export default function Chatroom({ location }) {
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    console.log("what is our location?", name);
+    console.log("what is my data?", room);
+  });
+
   return <div>This will be the Chatroom</div>;
 }
--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Join() {
   const [name, set_name] = useState("");
@@ -8,7 +8,7 @@ export default function Join() {
   return (
     <div className="joinOuterContainer">
       <div className="JoinInnerContainer">
-        <h1 classname="heading">fill this in to continue:</h1>
+        <h1 className="heading">fill this in to continue:</h1>
         <div>
           <input
             placeholder="User name"
@@ -25,15 +25,15 @@ export default function Join() {
             onChange={(event) => set_room(event.target.value)}
           />
         </div>
-        <link
+        <Link
           onClick={(event) => (!name || !room ? event.preventDefault() : null)}
           to={`/chatroom?name=${name}&room=${room}`}
         >
-          <button classname="button" type="submit">
+          <button className="button" type="submit">
             {" "}
             Join the chat!
           </button>
-        </link>
+        </Link>
       </div>
     </div>
   );

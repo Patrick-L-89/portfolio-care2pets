fe66e7a49a3e6f8dc7b90c98fc88a64b5c807012
patrick-coda <louter.p.h.a@gmail.com>
2020-09-07 15:57:38 +0200

    added the standard css for the infobar

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -3,6 +3,8 @@ import React from "react";
 import onlineIcon from "../../Icons/onlineIcon.png";
 import closeIcon from "../../Icons/closeIcon.png";
 
+import "./InfoBar.css";
+
 export default function InfoBar({ room }) {
   return (
     <div className="infoBar">

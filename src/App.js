6c819b34d99ba243784a93ed1349aded59355160
patrick-coda <louter.p.h.a@gmail.com>
2020-09-08 11:18:01 +0200

    changed navigation to the definitive version

--- src/App.js
+++ src/App.js
@@ -2,31 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import Navbar from "./components/Navbar";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
-import Navigation from "./components/Navigation/Navigation";
-import Homepage from "./pages/Homepage/Homepage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Join from "./components/Join/Join";
-import Chatroom from "./components/Chatroom/Chatroom";
 
 function App() {
   return (
@@ -37,7 +14,6 @@ function App() {
           <Route exact path="/" component={Homepage} />
         </Switch>
       </div>
->>>>>>> Stashed changes
     </div>
   );
 }

300252369cda20199c7cb2c6e0c05e2da50ed6ac
patrick-coda <louter.p.h.a@gmail.com>
2020-09-08 11:44:02 +0200

    added an about page for visitors who want to know what the page is about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./App.css";
-import Navbar from "./components/Navbar";
 import { Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/Homepage/Homepage";
+import Aboutpage from "./pages/About/About";
 
 function App() {
   return (
@@ -11,7 +10,8 @@ function App() {
       <Navigation />
       <div>
         <Switch>
-          <Route exact path="/" component={Homepage} />
+          <Route exact path="/home" component={Homepage} />
+          <Route exact path="/about" component={Aboutpage} />
         </Switch>
       </div>
     </div>
---
+++ src/pages/About/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        We here at care 2 pets want to give you the best possible care for your
+        pets. That's why on this site it is possible to search for pet owners
+        and ask the for advice. But youc an do more. If you cannot take care of
+        your pet for a while, due to sickness or maybe a holiday, you need
+        someone to take care of them. Who better than other people who own the
+        same pets?{" "}
+      </p>
+      <p>
+        Furthermore, if your pet is not by your side, you can chat with it's
+        current caretaker. Or mayby you just want some advice on how to take
+        best care of your pet. Just hop on to our chatfolder and start talking.{" "}
+      </p>
+    </div>
+  );
+}

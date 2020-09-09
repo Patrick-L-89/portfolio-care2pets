a7c08b7d92881761aa20da3d9d5863c2ebbfc8b4
patrick-coda <louter.p.h.a@gmail.com>
2020-09-09 23:38:04 +0200

    fixed a bug where the scrolltobottom would not work properly

--- src/pages/Chat-app/Messages/Messages.js
+++ src/pages/Chat-app/Messages/Messages.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import Message from "../Message/Message";
+import "./Messages.css";
 
-export default function Messages({ messages, name }) {
-  return (
-    <ScrollToBottom className="messages">
-      {messages.map((message, index) => (
-        <div key={index}>
-          <Message message={message} name={name} />
-        </div>
-      ))}
-    </ScrollToBottom>
-  );
-}
+const Messages = ({ messages, name }) => (
+  <ScrollToBottom className="messages">
+    {messages.map((message, i) => (
+      <div key={i}>
+        <Message message={message} name={name} />
+      </div>
+    ))}
+  </ScrollToBottom>
+);
+
+export default Messages;
